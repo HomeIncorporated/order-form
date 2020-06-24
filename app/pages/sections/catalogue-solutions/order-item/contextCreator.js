13325058b59e52961817889a921f514f1bb97831
Fawad Ali <alibombaye@gmail.com>
2020-06-24 14:08:57 +0100

    WIP

--- app/pages/sections/catalogue-solutions/order-item/contextCreator.js
+++ app/pages/sections/catalogue-solutions/order-item/contextCreator.js
@@ -1,5 +1,6 @@
 import manifest from './manifest.json';
 import { baseUrl } from '../../../../config';
+import { getSectionErrorContext } from '../../getSectionErrorContext';
 
 export const populateEstimationPeriod = ((selectedPrice) => {
   manifest.questions.estimationPeriod.options.forEach((option, i) => {
@@ -27,4 +28,39 @@ export const getContext = ({
     deleteButtonHref: '#',
     backLinkHref: `${baseUrl}/organisation/${orderId}/catalogue-solutions/select/solution/recipient`,
   });
+};
+
+const addErrorsToTableQuestions = ({ updatedManifest, errors }) => {
+  const manifestWithErrors = { ...updatedManifest };
+  const foundError = errors.find(error => error.field === updatedManifest.addPriceTable.data[0][0].question.id);
+  if (foundError) {
+    const errorMessage = updatedManifest.errorMessages[foundError.id];
+    manifestWithErrors.addPriceTable.data[0][1].error = { message: errorMessage };
+  }
+  return manifestWithErrors;
+};
+
+const errors = [
+  {
+    field: 'price-input-id',
+    id: 'PriceInputRequired',
+  },
+];
+
+export const getErrorContext = ({
+  params,
+}) => {
+  const updatedManifest = getContext({
+    orderId: params.orderId,
+    solutionName: params.solutionName,
+    serviceRecipientName: params.serviceRecipientName,
+    odsCode: params.odsCode,
+    selectedPrice: params.selectedPrice,
+  });
+
+  const updatedManifest2 = getSectionErrorContext({ ...params, manifest: updatedManifest });
+
+  return {
+    ...addErrorsToTableQuestions({ ...params, manifest: updatedManifest2 }),
+  };
 };
